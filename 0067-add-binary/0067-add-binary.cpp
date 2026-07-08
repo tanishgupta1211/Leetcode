2e2fa9f86ad035f167fb32a7a22e525edd155348
tanishgupta1211 <tanish00008@gmail.com>
2026-07-08 17:13:46 +0530

    Time: 3 ms (19.52%), Space: 11.2 MB (10.61%) - LeetHub

--- 0067-add-binary/0067-add-binary.cpp
+++ 0067-add-binary/0067-add-binary.cpp
@@ -1,13 +1,10 @@
 class Solution {
 public:
     string addBinary(string a, string b) {
-
         int i = a.size() - 1;
         int j = b.size() - 1;
-
         int carry = 0;
         string ans = "";
-
         while (i >= 0 || j >= 0 || carry) {
             int sum = carry;
             if (i >= 0) {
