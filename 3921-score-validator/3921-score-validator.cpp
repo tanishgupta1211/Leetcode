fd0d1577c97e5f996737737f22772b65da5b2ada
tanishgupta1211 <tanish00008@gmail.com>
2026-07-09 11:25:08 +0530

    Time: 4 ms (13.18%), Space: 55 MB (5.25%) - LeetHub

--- 3921-score-validator/3921-score-validator.cpp
+++ 3921-score-validator/3921-score-validator.cpp
@@ -1,32 +1,33 @@
 class Solution {
 public:
-    int getScore(const string &event) {
-        if (event == "1") return 1;
-        if (event == "2") return 2;
-        if (event == "3") return 3;
-        if (event == "4") return 4;
-        if (event == "6") return 6;
-        return 0;
-    }
-
     vector<int> scoreValidator(vector<string>& events) {
-        int score = 0, wickets = 0;
-        for (string &event : events) {
-            
+
+        unordered_map<string, int> runs = {
+            {"1", 1},
+            {"2", 2},
+            {"3", 3},
+            {"4", 4},
+            {"6", 6},
+            {"NB", 1},
+            {"WD", 1}
+        };
+
+        int score = 0;
+        int wickets = 0;
+
+        for (const string &event : events) {
+
             if (wickets == 10)
                 break;
 
-            int runs = getScore(event);
-            if (runs > 0) {
-                score += runs;
-            }
-            else if (event == "W") {
+            if (event == "W") {
                 wickets++;
             }
-            else if (event == "NB" || event == "WD") {
-                score++;
+            else {
+                score += runs[event];
             }
         }
+
         return {score, wickets};
     }
 };
