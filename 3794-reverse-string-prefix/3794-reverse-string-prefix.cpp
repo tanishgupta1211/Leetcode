5ac53f27f331f74383cd0a654f81753ab9a3347b
tanishgupta1211 <tanish00008@gmail.com>
2026-07-16 08:34:45 +0530

    Time: 0 ms (100%), Space: 8.7 MB (83.68%) - LeetHub

---
+++ 3794-reverse-string-prefix/3794-reverse-string-prefix.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string reversePrefix(string s, int k) {
+        int left = 0, right = k -1;
+        while(left < right){
+            swap(s[left], s[right]);
+            left++;
+            right--;
+        }
+        return s;
+    }
+};
