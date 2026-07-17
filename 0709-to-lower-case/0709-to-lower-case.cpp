290dfa9bc3f2417f0913f99e1a73b657f832d16e
tanishgupta1211 <tanish00008@gmail.com>
2026-07-17 21:37:39 +0530

    Time: 0 ms (100%), Space: 8.2 MB (83.51%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.cpp
+++ 0709-to-lower-case/0709-to-lower-case.cpp
@@ -3,7 +3,8 @@ public:
     string toLowerCase(string s) {
         for(int i = 0; i < s.size(); i++){
             if(s[i] >= 'A' && s[i] <= 'Z')
-                s[i] = tolower(s[i]);
+                s[i] = s[i] - 'A' + 'a';
+                //s[i] = tolower(s[i]);
         }
         return s;
     }
