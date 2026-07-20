b2794dd98d070bb064b0ea119e126fe3ba123279
tanishgupta1211 <tanish00008@gmail.com>
2026-07-20 18:21:21 +0530

    Time: 4 ms (6.26%), Space: 10.3 MB (78%) - LeetHub

---
+++ 0830-positions-of-large-groups/0830-positions-of-large-groups.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<int>> largeGroupPositions(string s) {
+
+        vector<vector<int>> ans;
+        int n = s.size();
+        int i = 0;
+        while (i < n) {
+            int j = i;
+            while (j < n && s[j] == s[i]) j++;
+            if (j - i >= 3) ans.push_back({i, j - 1});
+            i = j;
+        }
+        return ans;
+    }
+};
