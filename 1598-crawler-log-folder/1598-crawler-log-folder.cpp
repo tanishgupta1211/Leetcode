b804402aa0fb32e7e6b3be3ad4404edd3c2143af
tanishgupta1211 <tanish00008@gmail.com>
2026-07-23 07:56:29 +0530

    Time: 0 ms (100%), Space: 13.9 MB (96.8%) - LeetHub

---
+++ 1598-crawler-log-folder/1598-crawler-log-folder.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+        int depth = 0;
+        for(string log : logs){
+            if(log == "./") continue;
+
+            else if(log == "../"){
+                if(depth > 0) depth--;
+            }
+
+            else depth++;
+        }
+        return depth;
+    }
+};
