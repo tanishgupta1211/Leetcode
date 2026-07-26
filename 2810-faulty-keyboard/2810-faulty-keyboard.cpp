45ec10e228629ecfdb4ba315f55a30c370b1bfea
tanishgupta1211 <tanish00008@gmail.com>
2026-07-26 15:46:16 +0530

    Time: 0 ms (100%), Space: 13.5 MB (5.22%) - LeetHub

--- 2810-faulty-keyboard/2810-faulty-keyboard.cpp
+++ 2810-faulty-keyboard/2810-faulty-keyboard.cpp
@@ -1,12 +1,32 @@
 class Solution {
 public:
     string finalString(string s) {
-        string ans = "";
+        deque<char> dq;
+        bool rev = false;
         for(char ch : s){
             if(ch == 'i'){
-                reverse(ans.begin(), ans.end());
+                rev = !rev;
             }
-            else ans += ch;
+            else{
+                if(!rev)
+                    dq.push_back(ch);
+                else
+                    dq.push_front(ch);
+            }
+        }
+        
+        string ans;
+        if(!rev){
+            while(!dq.empty()){
+                ans += dq.front();
+                dq.pop_front();
+            }
+        }
+        else{
+            while(!dq.empty()){
+                ans += dq.back();
+                dq.pop_back();
+            }
         }
         return ans;
     }
