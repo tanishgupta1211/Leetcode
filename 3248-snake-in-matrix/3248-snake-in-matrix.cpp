de78ef13e70896f6d7e21c098c9792c749fbb98b
tanishgupta1211 <tanish00008@gmail.com>
2026-07-28 08:25:04 +0530

    Time: 0 ms (100%), Space: 34.9 MB (29.88%) - LeetHub

---
+++ 3248-snake-in-matrix/3248-snake-in-matrix.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int finalPositionOfSnake(int n, vector<string>& commands) {
+        int row = 0, col = 0;
+        for(string &cmd : commands){
+            if(cmd == "UP") row--;
+            else if(cmd == "DOWN") row++;
+            else if(cmd == "LEFT") col--;
+            else col++;
+        }
+        return row * n + col;
+    }
+};
