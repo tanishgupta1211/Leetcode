251c3e84a63666ebcc09132f0fa7a13b32e2e510
tanishgupta1211 <tanish00008@gmail.com>
2026-07-29 08:28:32 +0530

    Time: 0 ms (100%), Space: 8.3 MB (85.42%) - LeetHub

---
+++ 3174-clear-digits/3174-clear-digits.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string clearDigits(string s) {
+        string ans;
+        for(char ch : s){
+            if(isdigit(ch)) ans.pop_back();
+            else ans.push_back(ch);
+        }
+        return ans;
+    }
+};
