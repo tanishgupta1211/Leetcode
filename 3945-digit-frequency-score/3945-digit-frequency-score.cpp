7e682b995d6192458dc021c4e911009d409fa129
tanishgupta1211 <tanish00008@gmail.com>
2026-08-05 00:53:29 +0530

    Time: 0 ms (100%), Space: 8.6 MB (57.65%) - LeetHub

--- 3945-digit-frequency-score/3945-digit-frequency-score.cpp
+++ 3945-digit-frequency-score/3945-digit-frequency-score.cpp
@@ -1,17 +1,11 @@
 class Solution {
 public:
     int digitFrequencyScore(int n) {
-        if(n == 0) return 0;
-        vector<int> freq(10, 0);
-        while(n > 0){
-            int digit = n % 10;
-            freq[digit]++;
+        int score = 0;
+        while (n > 0) {
+            score += n % 10;
             n /= 10;
         }
-        int ans = 0;
-        for(int i = 0; i < 10; i++){
-            ans += (i * freq[i]);
-        }
-        return ans;
+        return score;
     }
 };
