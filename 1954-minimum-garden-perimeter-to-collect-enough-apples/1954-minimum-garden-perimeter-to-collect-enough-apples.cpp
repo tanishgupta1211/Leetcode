f0ad3f9bddaf1a14fcd8de4210a46105fdc52f5c
tanishgupta1211 <tanish00008@gmail.com>
2026-08-22 22:32:04 +0530

    Time: 0 ms (100%), Space: 8.2 MB (14.61%) - LeetHub

--- 1954-minimum-garden-perimeter-to-collect-enough-apples/1954-minimum-garden-perimeter-to-collect-enough-apples.cpp
+++ 1954-minimum-garden-perimeter-to-collect-enough-apples/1954-minimum-garden-perimeter-to-collect-enough-apples.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
     long long minimumPerimeter(long long neededApples) {
-        long long k = 1;
-        while (2*k*(k+1)*(2*k+1) < neededApples) {
-            k++;
+        long long low = 1, high = 100000;
+        while (low < high){
+            long long mid = low + (high-low)/2;
+            long long apples = 2 * mid * (mid+1) * (2*mid+1);
+            if (apples >= neededApples) high = mid;
+            else low = mid + 1;
         }
-        return 8*k;
+        return 8*low;
     }
 };
