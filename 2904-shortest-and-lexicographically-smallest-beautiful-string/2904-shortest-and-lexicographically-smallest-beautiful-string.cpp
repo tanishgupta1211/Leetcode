e745ae431b2b071ccc9a07adea8355b57cccce88
tanishgupta1211 <tanish00008@gmail.com>
2026-08-26 09:28:53 +0530

    Time: 0 ms (100%), Space: 8.8 MB (64.74%) - LeetHub

--- 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -4,16 +4,16 @@ public:
         int n = s.size();
         string ans = "";
         for(int i = 0; i < n; i++){
-            for (int j = i; j < n; j++){
-                int cnt = 0;
-                for(int x = i; x <= j; x++){
-                    if (s[x] == '1') cnt++;
-                }
-                if (cnt == k) {
+            int cnt = 0;
+            for(int j = i; j < n; j++){
+                if(s[j] == '1') cnt++;
+                if(cnt > k) break;
+                if(cnt == k) {
                     string curr = s.substr(i, j - i + 1);
-                    if (ans == "" || curr.size() < ans.size() || (curr.size() == ans.size() && curr < ans)){
+                    if(ans == "" || curr.size() < ans.size() || (curr.size() == ans.size() && curr < ans)){
                         ans = curr;
                     }
+                    break;
                 }
             }
         }
