764b68952465b2fe6c78f20a5bc3a279f97991e1
tanishgupta1211 <tanish00008@gmail.com>
2026-09-06 23:25:41 +0530

    Time: 3 ms (14.2%), Space: 12.9 MB (16.09%) - LeetHub

--- 0274-h-index/0274-h-index.cpp
+++ 0274-h-index/0274-h-index.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
-    int hIndex(vector<int>& nums) {
-        int maxi = INT_MIN;
-        for (int i = 0; i < nums.size(); i++)
-            maxi = max(maxi, nums[i]);
-        int maxCnt = 0;
-        for(int h = 1; h <= maxi; h++){
-            int cnt = 0;
-            for(int i = 0; i < nums.size(); i++){
-                if(nums[i] >= h) cnt++;
-            }
-            if (cnt >= h) maxCnt = h;
+    int hIndex(vector<int>& citations) {
+        int n = citations.size();
+        vector<int> cnt(n + 1, 0);
+        for(int c : citations)
+            cnt[min(c, n)]++;
+        int papers = 0;
+        for (int h = n; h >= 0; h--) {
+            papers += cnt[h];
+            if (papers >= h) return h;
         }
-        return maxCnt;
+        return 0;
     }
 };
