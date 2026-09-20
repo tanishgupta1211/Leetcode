0315390e0b190ebe65b2f06db08a6b94f0ea7fd6
tanishgupta1211 <tanish00008@gmail.com>
2026-09-20 10:27:13 +0530

    Time: 0 ms (100%), Space: 9.8 MB (15.78%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
@@ -3,9 +3,8 @@ public:
     int reverseDegree(string s) {
         int n = s.size();
         long long degree = 0;
-        for(int i = 1; i <= n; i++){
-            degree += 1ll * i * ('z' - s[i-1] + 1);
-        }
+        for(int i = 1; i <= n; i++)
+            degree += 1ll * i * ('z'-s[i-1]+1);
         return degree;
     }
 };
